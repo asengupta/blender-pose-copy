63ef783fdee76c6d1f2fedb2bc8f482f63334347
Ettenmoor <ettenmoor@gmail.com>
2024-04-11 04:28:51 +0530

    Refactoring to allow posing one-offs

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-This is a simple Blender script which retargets the animation from a skeleton lacking a rest pose to an identical skeleton which does have a rest pose (but not animations, obviously). At the end, the rest pose-equipped armature will have the animations transferred to it.
+This is a simple Blender script which can work with bone trees, for adding and deleting keyframes en-masse. It was originally written to retarget the animation from a skeleton lacking a rest pose to an identical skeleton which does have a rest pose (but not animations, obviously). At the end, the rest pose-equipped armature will have the animations transferred to it.
 
 I wrote this out of a very specific problem: Elden Ring animations imported into Blender had an incoherent rest pose, which I wanted to fix.
 This script could potentially be used to retarget similar or incompatible skeletons if a bone mapping was provided.
 
-This script can also pose arbitrary bones (including all child bone poses) given a source and a target. This makes it useful in remixing parts of multiple existing animations into a new one (with a little bit of tweaking after importing, of course).
-
-THe script you want to run is ```pose-copy.py```.
+The script you want to run is ```pose-copy.py```.
 
 You will need to modify this script in the following way to use it effectively:
 
@@ -21,8 +19,20 @@ Also modify the following line:
 build_keyframes([ARRAY_OF_FRAME_NUMBERS])
 ```
 
-You may choose to forgo building keyframes altogether, and simply choose to pose the armature (or part of it). In that case, change the frame to the target armature (which already has the animation) or pose it however you like, and then use:
+You may choose to forgo building keyframes altogether, and simply choose to pose the armature (or part of it) in line with an existing animation frame. In that case, change the frame to the target armature (which already has the animation) or pose it however you like, and then use:
 
 ```
 pose_once(ROOT_BONE_ID)
 ```
+
+There is other ancilliary functionality present. Here are some ways in which you can delete keyframes:
+
+```
+# Deletes all keyframes of a bone and its children
+delete_keyframe(source_armature, ROOT_BONE_ID, hierarchy(source_bones[ROOT_BONE_ID]), ALL_FRAMES)
+
+# Deletes all keyframes of a bone and its children between Frames 9 and 10 (keyframe exactly at Frame 9 is not deleted, keyframe at Frame 10 is deleted)
+delete_keyframe(source_armature, ROOT_BONE_ID, hierarchy(source_bones[ROOT_BONE_ID]), SINGLE_FRAME(10))
+```
+
+You can write your own delete condition by simply implementing a lambda (see ALL_FRAMES and SINGLE_FRAME implementations for examples), and passing it into ```delete_keyframe()```.
--- pose-copy.py
+++ pose-copy.py
@@ -4,9 +4,9 @@ import bpy
 
 bpy.app.handlers.frame_change_post.clear()
 
-SOURCE_ARMATURE_NAME = "Rest Pose Armature.Original.001"
+SOURCE_ARMATURE_NAME = "Block Transition.Original.001"
 TARGET_ARMATURE_NAME = "Spear Walk.001"
-ROOT_BONE_ID = "Master"
+ROOT_BONE_ID = "R_UpperArm"
 
 source_armature = bpy.data.objects.get(SOURCE_ARMATURE_NAME)
 target_armature = bpy.data.objects.get(TARGET_ARMATURE_NAME)
@@ -50,19 +50,75 @@ def pose_internal(bone_name, root_bone_tree):
     clear_pose()
     pose_recursively(bone_name, root_bone_tree)
 
-def insert_keyframe(armature, frame):
-    for bone in armature.pose.bones:
-        bone.keyframe_insert(data_path="location", frame = frame)
-        bone.keyframe_insert(data_path="rotation_quaternion", frame = frame)
+def SINGLE_FRAME(frame):
+    return lambda kf: (kf.co.x > frame -1 and kf.co.x <= frame)
+
+def ALL_FRAMES(kf):
+    return True
+ 
+def delete_keyframe_with_property(property, bone, armature, delete_condition):
+    data_path = "pose.bones[\"{}\"].{}".format(bone.name, property)
+    print("Searching for {}".format(data_path))
+    fcurves = armature.animation_data.action.fcurves
+    bone_fcurves = [fcu for fcu in fcurves if fcu.data_path == data_path]
+    for fcurve in bone_fcurves:
+        print("Processing {}".format(fcurve.data_path))
+#        points = [kf for kf in fcurve.keyframe_points if (kf.co.x > frame -1 and kf.co.x <= frame)]
+        points = [kf for kf in fcurve.keyframe_points if delete_condition(kf)]
+        while len(points) > 0:
+            print("Deleting {}".format(points[0].co))
+            fcurve.keyframe_points.remove(points[0])
+            fcurve.keyframe_points.handles_recalc()
+            points = [kf for kf in fcurve.keyframe_points if delete_condition(kf)]
+#            points = [kf for kf in fcurve.keyframe_points if (kf.co.x > frame -1 and kf.co.x <= frame)]
+#        for kf in fcurve.keyframe_points:
+#            if (kf.co.x > frame -1 and kf.co.x <= frame):
+#                print("Deleting a keyframe: {}".format(kf.co))
+#                fcurve.keyframe_points.remove(kf)
+#                fcurve.keyframe_points.handles_recalc()
+#                fcurve.update()
+
+def delete_bone_keyframe(bone, armature, delete_condition):
+    delete_keyframe_with_property("location", bone, armature, delete_condition)
+    delete_keyframe_with_property("rotation_quaternion", bone, armature, delete_condition)
+
+def insert_keyframe_recursively(frame, bone_tree, armature):
+    for child_bone_name, child_bone_tree in bone_tree.items():
+        child_bone = armature.pose.bones[child_bone_name]
+        child_bone.keyframe_insert(data_path="location", frame = frame)
+        child_bone.keyframe_insert(data_path="rotation_quaternion", frame = frame)
+        insert_keyframe_recursively(frame, child_bone_tree, armature)
+
+def delete_keyframe_recursively(bone_tree, armature, delete_condition):
+    for child_bone_name, child_bone_tree in bone_tree.items():
+        child_bone = armature.pose.bones[child_bone_name]
+        delete_bone_keyframe(child_bone, armature, delete_condition)
+        delete_keyframe_recursively(child_bone_tree, armature, delete_condition)
+
+def delete_keyframe(armature, root_bone_name, root_bone_tree, delete_condition):
+    root_bone = armature.pose.bones[root_bone_name]
+    delete_bone_keyframe(root_bone, armature, delete_condition)
+    delete_keyframe_recursively(root_bone_tree, armature, delete_condition)
+
+def insert_keyframe(armature, root_bone_name, root_bone_tree, frame):
+    root_bone = armature.pose.bones[root_bone_name]
+    root_bone.keyframe_insert(data_path="location", frame = frame)
+    root_bone.keyframe_insert(data_path="rotation_quaternion", frame = frame)
+    insert_keyframe_recursively(frame, root_bone_tree, armature)
+
+#def clear_animation(armature, root_bone_name):
+#    root_bone_tree = hierarchy(source_bones[root_bone_name])
+#    delete_keyframe(source_armature, root_bone_name, root_bone_tree, frame)
 
 def keyframe_insert_frame_handler_builder(root_bone_name):
     root_bone_tree = hierarchy(source_bones[root_bone_name])
     def keyframe_insert_frame_handler(scene):
         frame = scene.frame_current
         print("Current frame is {}".format(frame))
+        delete_keyframe(source_armature, root_bone_name, root_bone_tree, SINGLE_FRAME(frame))
         pose_internal(root_bone_name, root_bone_tree)
-        bpy.ops.object.mode_set(mode='OBJECT')
-        insert_keyframe(source_armature, frame)
+#        bpy.ops.object.mode_set(mode='OBJECT')
+        insert_keyframe(source_armature, root_bone_name, root_bone_tree, frame)
     return keyframe_insert_frame_handler
 
 def deselect_all():
@@ -76,12 +132,11 @@ def build_keyframes(frame_numbers):
 handler = keyframe_insert_frame_handler_builder(ROOT_BONE_ID)
 bpy.app.handlers.frame_change_post.append(handler)
 
-#root_bone_tree = hierarchy(source_bones[ROOT_BONE_ID])
-
 deselect_all()
 
 #pose_once(ROOT_BONE_ID)
-build_keyframes(range(1,45))
+delete_keyframe(source_armature, ROOT_BONE_ID, hierarchy(source_bones[ROOT_BONE_ID]), ALL_FRAMES)
+build_keyframes([0])
 
 print("Unregistering handler")
 bpy.app.handlers.frame_change_post.clear()
